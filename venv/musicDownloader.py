027eff387144e91cbb9c273ff22087b0c25990de
Shubham Pandey <shubham.is.here@outlook.com>
2019-05-27 11:08:47 +0530

    insert arguments through ini file, comments added.

---
+++ venv/musicDownloader.py
@@ -0,0 +1,56 @@
+"""   Use Python 3 to run script.
+REFERENCES-> youtube_dl: https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/YoutubeDL.py
+          -> BeautifulSoup4: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+          -> requests: https://realpython.com/python-requests/ & https://www.geeksforgeeks.org/get-post-requests-using-python/
+          -> ffprobe3: https://github.com/DheerendraRathor/ffprobe3   """
+
+
+from __future__ import unicode_literals
+from configparser import ConfigParser
+import bs4
+import requests
+import os
+import sys
+import time
+import youtube_dl
+
+parser = ConfigParser()
+parser.read('input.ini')
+
+#if len(sys.argv) == 1:
+#    print("Invalid Command. Enter Search Terms.")
+#    sys.exit()
+
+home_url = "https://www.youtube.com"
+
+os.makedirs("Music", exist_ok=True)
+#search_term = ' '.join(sys.argv[1:])
+url = "https://www.youtube.com/results?search_query=" + parser.get('default', 'search_term')
+
+res = requests.get(url)
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text)
+
+#print("Not the actual soup: " + soup)
+
+search_results = soup.select("h3 a")
+#print(search_results)
+video_title = search_results[0].get("title")
+video_url = home_url + search_results[0].get("href")
+print("This is video URL: " + search_results[0].get("href"))
+print("Downloading audio from %s" % video_url)
+
+os.chdir("Music")
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([video_url])
+
+print("Download Complete.")
